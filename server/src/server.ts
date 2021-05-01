9538cacc2897286fe4fe88d9f5fc5e863a06c199
NirmalHegde <nirmalhegde821@gmail.com>
2021-05-01 15:16:56 -0400

    express initialization

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,14 @@
-console.log("working");
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+app.get('/', (req, res) => {
+  res.send('The sedulous hyena ate the antelope!');
+});
+app.listen(port, err => {
+  if (err) {
+    return console.error(err);
+  }
+  return console.log(`server is listening on ${port}`);
+});
