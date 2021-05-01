1d0be958e6dd7956e21a2b61f0e76ccd1653c0bd
NirmalHegde <nirmalhegde821@gmail.com>
2021-05-01 16:10:25 -0400

    eslint init

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,9 +3,11 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
 app.get('/', (req, res) => {
   res.send('The sedulous hyena ate the antelope!');
 });
+
 app.listen(port, err => {
   if (err) {
     return console.error(err);
